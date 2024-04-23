0a781e49e6e1864b883fd275c9111612926a3c41
jeanbr2020 <61910959+jeanbr2020@users.noreply.github.com>
2024-04-22 21:20:07 -0300

    Add files via upload

---
+++ FormsPHP/pages/politica.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="styles\Styles.css">
+    <link rel="shortcut icon" type="imagex/png" href="../img/5949930.png">
+    <title>Politica e Privacidade</title>
+    <style>
+        body {
+            background-color: #f8f9fa;
+        }
+        .container {
+            max-width: 800px;
+            margin: auto;
+            padding: 20px;
+        }
+        .card {
+            margin-bottom: 20px;
+            border-radius: 5px;
+            box-shadow: 0 2px 5px rgba(0,0,0,0.15);
+        }
+        .btn-back {
+            margin-bottom: 20px;
+        }
+    </style>
+</head>
+<body class="poli">
+    <div id="estilo" class="container">
+       
+        <h1>Politica de Privacidade</h1>
+        <div class="card">
+            <div class="card-body">
+                <h2 class="card-title">1. Introdução</h2>
+                <p class="card-text">Bem-vindo ao Crud. Respeitamos a privacidade de nossos usuários e estamos comprometidos em proteger as informações pessoais que você compartilha conosco em conformidade com as leis de privacidade e proteção de dados aplicáveis.</p>
+            </div>
+            <div class="card-body">
+                <h2 class="card-title">2. Informações que coletamos</h2>
+                <p class="card-text">Podemos coletar informações pessoais que você nos fornece voluntariamente, como seu nome, endereço de e-mail, número de telefone, etc.</p>
+            </div>
+            <div class="card-body">
+                <h2 class="card-title">3. Como usamos suas informações</h2>
+                <p class="card-text">Usamos suas informações para fornecer, manter e melhorar nossos serviços, bem como para fins administrativos e de marketing.</p>
+            </div>
+            <div class="card-body">
+                <h2 class="card-title">4. Compartilhamento de informações</h2>
+                <p class="card-text">Não compartilhamos suas informações pessoais com terceiros, exceto conforme descrito nesta política de privacidade.</p>
+            </div>
+            <div class="card-body">
+                <h2 class="card-title">5. Segurança</h2>
+                <p class="card-text">Estamos comprometidos em proteger suas informações pessoais e implementamos medidas de segurança razoáveis para proteger suas informações.</p>
+            </div>
+            <div class="card-body">
+                <h2 class="card-title">6. Seus direitos</h2>
+                <p class="card-text">Você tem o direito de acessar, corrigir ou excluir suas informações pessoais a qualquer momento.</p>
+            </div>
+            <div class="card-body">
+                <h2 class="card-title">7. Alterações à nossa política de privacidade</h2>
+                <p class="card-text">Podemos atualizar nossa política de privacidade de tempos em tempos. Notificaremos você sobre quaisquer alterações, publicando a nova política de privacidade nesta página.</p>
+            </div>
+            <div class="card-body">
+                <h2 class="card-title">8. Contate-Nos</h2>
+                <p class="card-text">Se você tiver alguma dúvida sobre esta política de privacidade, entre em contato conosco.</p>
+            </div>
+        </div>
+
+
+        <!-- arrumar caminho para index.php -->
+        <a href="http://localhost/Programacao_Web/FormsPHP/" class="btn btn-primary btn-back">Voltar a pagina inicial</a>
+
+    </div>
+
+
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="js\Main.js"></script>
+</body>
+</html>
